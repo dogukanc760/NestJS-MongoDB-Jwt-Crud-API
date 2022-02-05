8053c72f751b027d7714f65a00607e5c1dbaf4c2
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2022-02-05 20:44:59 +0300

    swagger ui added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AllExceptionFilter } from 'libs/filters/all-exception.filters';
 import { AppModule } from './app.module';
 
@@ -8,6 +9,14 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   // this for global url, for example : localhost:3000/api/
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+  .setTitle('NestJS Crud API Swagger UI')
+  .setDescription('NestJS-MongoDB-JWT Auth-Open API')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
